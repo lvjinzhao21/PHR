a64de6d6817965896a129410e948bdf21017188e
lvjinzhao <lv_jinzhao@163.com>
2018-06-04 17:57:10 +0800

    初始化项目

---
+++ src/main/java/com/qybl/phr/PhrApplication.java
@@ -0,0 +1,12 @@
+package com.qybl.phr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhrApplication.class, args);
+    }
+}
